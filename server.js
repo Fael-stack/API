fa3e27e149e0408705996a2796b9bcd8a86bcec6
Dev Aze <azevchk@gmail.com>
2025-03-28 23:06:27 -0300

    add put route and need to fix delete

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 const app = express();
 
@@ -6,13 +9,47 @@ app.use(express.json());
 
 const users = [];
 
-app.post("/users", (req, res) => {
+app.post("/users", async (req, res) => {
+  await prisma.user.create({
+    data: {
+      name: req.body.name,
+      email: req.body.email,
+      age: req.body.age,
+    },
+  });
+
   users.push(req.body);
 
   res.status(201).json(req.body);
 });
 
-app.get("/users", (req, res) => {
+app.put("/users/:id", async (req, res) => {
+  await prisma.user.update({
+    where: {
+      id: req.params.id,
+    },
+    data: {
+      name: req.body.name,
+      email: req.body.email,
+      age: req.body.age,
+    },
+  });
+
+  app.delete("/users/:id", async (req, res) => {
+    await prisma.user.delete({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json({ message: "usuario deletado" });
+  });
+
+  res.status(201).json(req.body);
+});
+
+app.get("/users", async (req, res) => {
+  const users = await prisma.user.findMany();
+
   res.status(200).json(users);
 });
 
